3f0927283606263c493d4eae85e840d91212664a
hua_bang <47221942+hua-bang@users.noreply.github.com>
2024-02-17 16:06:06 +0800

    ✨ feat(todo): support AppConfigGuard

---
+++ notion-helper-server/src/common/guards/app-config.ts
@@ -0,0 +1,29 @@
+// config.guard.ts
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+
+@Injectable()
+export class AppConfigGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    const appSecret = process.env.APP_SECRET;
+    const requestSecret = request.headers['request_secret']; // 假设密钥通过请求头传递
+
+    if (!appSecret) {
+      throw new UnauthorizedException(
+        'appSecret is not configured in the environment variables.',
+      );
+    }
+
+    if (appSecret !== requestSecret) {
+      // 如果appSecret和请求中的requestSecret不匹配，抛出异常
+      throw new UnauthorizedException('Invalid request secret.');
+    }
+
+    return true; // 如果检查通过，则继续执行
+  }
+}
--- notion-helper-server/src/notion/notion.controller.ts
+++ notion-helper-server/src/notion/notion.controller.ts
@@ -1,17 +1,20 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { NotionService } from './notion.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
+import { AppConfigGuard } from 'src/common/guards/app-config';
 
 @Controller('notion')
 export class NotionController {
   constructor(private readonly notionService: NotionService) {}
 
   @Get('/todo/list')
+  @UseGuards(AppConfigGuard)
   getTodoList() {
     return this.notionService.getTodoList();
   }
 
   @Post('/todo/add')
+  @UseGuards(AppConfigGuard)
   addTodo(@Body() todo: CreateTodoDto) {
     return this.notionService.addTodo(todo);
   }
