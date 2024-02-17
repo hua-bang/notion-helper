946009b627ecc107a9da785e1354af816b76124f
hua_bang <47221942+hua-bang@users.noreply.github.com>
2024-02-17 16:22:29 +0800

    ✨ feat(config): support custom port

--- notion-helper-server/src/main.ts
+++ notion-helper-server/src/main.ts
@@ -5,6 +5,6 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000);
+  app.listen(process.env.PORT || 4000);
 }
 bootstrap();
